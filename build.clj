135458628dd5f28c03e89369846c684e3b110180
janos erdos <erdosj@gmail.com>
2024-01-07 11:43:04 +0100

    work on building

--- build.clj
+++ build.clj
@@ -2,9 +2,20 @@
   (:require [clojure.tools.build.api :as b]))
 
 (def lib 'dev.erdos/riall)
-(def version (format "1.2.%s" (b/git-count-revs nil)))
 (def class-dir "target/classes")
 (def basis (b/create-basis {:project "deps.edn"}))
+
+;; compare version value to previous commit, set RELEASE_VERSION only when changed.
+(def version
+  (let [stable   (-> basis :version :stable)
+        stable-1 (-> (b/git-process {:git-args ["show" "HEAD~:deps.edn"]}) read-string :version :stable)]
+    (if (= stable stable-1)
+      (-> basis :version :snapshot)
+      (let [envfile (System/getenv "GITHUB_ENV")]
+        (println :bumping-version stable-1)
+        (spit envfile (format "RELEASE_VERSION=%s\n" stable) :append true)
+        stable))))
+
 (def uber-file (format "target/%s-%s-standalone.jar" (name lib) version))
 (def ns-main 'riall.core)
 
@@ -13,20 +24,28 @@
 
 (defn uber [_]
   (clean nil)
-  (b/copy-dir {:src-dirs ["src" "resources"]
-               :target-dir class-dir})
+  (b/copy-dir {:src-dirs ["src"] :target-dir class-dir})
+  (spit "target/classes/riall/core.cljc"
+        (format "(def version \"%s\")" version)
+        :append true)
   (b/compile-clj {:basis basis
                   :ns-compile [ns-main]
                   :class-dir class-dir
-                  :compile-opts {:elide-meta [:doc :file :line] :direct-linking true}
-                  })
+                  :compile-opts {:elide-meta [:doc :file :line] :direct-linking true}})
   (b/uber {:class-dir class-dir
            :uber-file uber-file
            :basis basis
-           :main ns-main}))
+           :main ns-main
+           ; :exclude [".*\\.cljc$" ".*\\.clj$" ".*pprint.*"]
+           }))
+
+(def native-image-cmd "native-image")
 
 (defn native [_]
   (uber nil)
-  ;;; TODO: call `native-image`
-
-)
+  (b/process {:out :inherit
+              :err :inherit
+              :command-args [native-image-cmd
+                             "--no-fallback" "--report-unsupported-elements-at-runtime" "--initialize-at-build-time"
+                             "-jar" uber-file
+                             "-o" "sankey"]}))
